21e8f014db76a5ed70499e423be2bb81090e37d6
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-08-23 23:00:38 +0200

    Jo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+
+// we need to store all past operations to allow to verify them
+// so we need a DAG (signed)
+// storing the DAG, querying the DAG, merging the DAG and calculating
+// the current states based on the changes should be efficient
+
 fn main() {
     println!("Hello, world!");
 }
