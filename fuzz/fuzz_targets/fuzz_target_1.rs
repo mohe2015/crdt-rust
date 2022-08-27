c13f278064a9412e29d3b75e1a724f8bcb70a5f9
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-08-27 22:07:03 +0200

    Jo

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,6 +1,8 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
+use crdt::RandomDAG;
+use crdt::topological_sort;
 
-fuzz_target!(|data: RandomDAG| {
-    topological_sort(data)
+fuzz_target!(|data: RandomDAG<i32>| {
+    topological_sort(data.0);
 });
