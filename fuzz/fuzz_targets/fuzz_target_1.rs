cce092cd945619706f6e267c48a698f3673ee532
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-08-27 23:24:13 +0200

    Jo

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -8,6 +8,14 @@ use crdt::RandomDAG;
 use crdt::topological_sort;
 use by_address::ByAddress;
 
+/*
+/home/moritz/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-cov show -Xdemangler=rustfilt fuzz/target/x86_64-unknown-linux-gnu/release/fuzz_target_1 \
+    -instr-profile=fuzz/coverage/fuzz_target_1/coverage.profdata \
+    -show-line-counts-or-regions \
+    -show-instantiations --format=html --output-dir=coverage/html
+*/
+
+
 fuzz_target!(|data: RandomDAG<i32>| {
     let result = topological_sort(data.0.clone());
     
@@ -17,6 +25,12 @@ fuzz_target!(|data: RandomDAG<i32>| {
     let b: BTreeSet<&ByAddress<Rc<RefCell<DAGNode<i32>>>>> = result.iter().collect();
 
     assert_eq!(a, b);
+
+    for i in 0..result.len() {
+        for predecessor in &result[i].borrow().predecessors {
+            assert!(result[0..i].contains(predecessor));
+        }
+    }
 
     // TODO check only references to earlier nodes
 });
